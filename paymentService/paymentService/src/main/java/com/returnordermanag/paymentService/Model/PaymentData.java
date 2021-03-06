0f0c5696d6d3e53bcd156575370aaceb3d904383
Ashutosh Gupta <aashutosh.g27@gmail.com>
2021-03-07 05:28:31 +0530

    Update PaymentData.java

--- paymentService/paymentService/src/main/java/com/returnordermanag/paymentService/Model/PaymentData.java
+++ paymentService/paymentService/src/main/java/com/returnordermanag/paymentService/Model/PaymentData.java
@@ -1,5 +1,43 @@
 package com.returnordermanag.paymentService.Model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PaymentData {
+	
+	private String CusName;
+	private String CreditCardNumber;
+	private int CreditLimit;
+	private float CurrentBalance;
+	
+	public String getCusName() {
+		return CusName;
+	}
+	public void setCusName(String cusName) {
+		CusName = cusName;
+	}
+	public String getCreditCardNumber() {
+		return CreditCardNumber;
+	}
+	public void setCreditCardNumber(String creditCardNumber) {
+		CreditCardNumber = creditCardNumber;
+	}
+	public int getCreditLimit() {
+		return CreditLimit;
+	}
+	public void setCreditLimit(int creditLimit) {
+		CreditLimit = creditLimit;
+	}
+	public float getCurrentBalance() {
+		return CurrentBalance;
+	}
+	public void setCurrentBalance(float currentBalance) {
+		CurrentBalance = currentBalance;
+	}
+	
+	public float ReturnCurrentBalnce (String CreditCardNumber) {
+		
+		return 120000;
+	}
 
 }
