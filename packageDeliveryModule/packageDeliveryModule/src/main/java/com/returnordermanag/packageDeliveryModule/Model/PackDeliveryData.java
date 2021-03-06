caa335b3db57de93fba27059f8223e3327b49611
Ashutosh Gupta <aashutosh.g27@gmail.com>
2021-03-07 04:46:47 +0530

    Update PackDeliveryData.java

--- packageDeliveryModule/packageDeliveryModule/src/main/java/com/returnordermanag/packageDeliveryModule/Model/PackDeliveryData.java
+++ packageDeliveryModule/packageDeliveryModule/src/main/java/com/returnordermanag/packageDeliveryModule/Model/PackDeliveryData.java
@@ -1,5 +1,45 @@
 package com.returnordermanag.packageDeliveryModule.Model;
 
-public class PackDeliveryData {
+import org.springframework.stereotype.Component;
 
+@Component
+public class PackDeliveryData {
+	
+	private int IntergalPackaging = 100;
+	private int IntergalDelivery = 200;
+	private int AccessoryPacking = 50;
+	private int AccessoryDelivery = 100;
+	private int ProtectiveSheath = 50;
+	
+	public int getIntergalPackaging() {
+		return IntergalPackaging;
+	}
+	public void setIntergalPackaging(int intergalPackaging) {
+		IntergalPackaging = intergalPackaging;
+	}
+	public int getIntergalDelivery() {
+		return IntergalDelivery;
+	}
+	public void setIntergalDelivery(int intergalDelivery) {
+		IntergalDelivery = intergalDelivery;
+	}
+	public int getAccessoryPacking() {
+		return AccessoryPacking;
+	}
+	public void setAccessoryPacking(int accessoryPacking) {
+		AccessoryPacking = accessoryPacking;
+	}
+	public int getAccessoryDelivery() {
+		return AccessoryDelivery;
+	}
+	public void setAccessoryDelivery(int accessoryDelivery) {
+		AccessoryDelivery = accessoryDelivery;
+	}
+	public int getProtectiveSheath() {
+		return ProtectiveSheath;
+	}
+	public void setProtectiveSheath(int protectiveSheath) {
+		ProtectiveSheath = protectiveSheath;
+	} 
+	
 }
