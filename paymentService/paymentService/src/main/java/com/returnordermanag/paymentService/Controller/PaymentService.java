c8efac63d52305dff61bdd270086e187ce5717d7
Ashutosh Gupta <aashutosh.g27@gmail.com>
2021-03-08 04:21:13 +0530

    Update PaymentService.java

--- paymentService/paymentService/src/main/java/com/returnordermanag/paymentService/Controller/PaymentService.java
+++ paymentService/paymentService/src/main/java/com/returnordermanag/paymentService/Controller/PaymentService.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class PaymentService {
 	@Autowired
 	private PaymentData paymentdata;
 
-	@RequestMapping("/ProcessPayment/{cCardNo}/{CreditLimit}/{ProcessCharge}")
+	@GetMapping("/ProcessPayment/{cCardNo}/{CreditLimit}/{ProcessCharge}")
 	public Map<String, String> returnUserBalance(@PathVariable("cCardNo") String cCardNo,
 			@PathVariable("CreditLimit") int CreditLimit, @PathVariable("ProcessCharge") float ProcessCharge) {
 		
